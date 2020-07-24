0d402dc1a2634221fa7a62b1bde74176c9b975be
xjchenhao <wy.chenhao@qq.com>
2020-07-24 13:54:28 +0800

    增加登出的接口

--- src/services/account.js
+++ src/services/account.js
@@ -1,9 +1,18 @@
 import request from 'utils/request';
 
-// 注册
+// 登录
 export function login(params) {
   return request({
     url: '/api/login',
+    type: 'post',
+    data: params,
+  });
+}
+
+// 登出
+export function logout(params) {
+  return request({
+    url: '/api/logout',
     type: 'post',
     data: params,
   });
