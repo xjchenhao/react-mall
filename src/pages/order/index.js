5c99cd50a038bb56bc9454ab180e3c99e0bea094
xjchenhao <wy.chenhao@qq.com>
2020-07-26 21:46:00 +0800

    【bug修复】订订单列表只获取了一次api

--- src/pages/order/index.js
+++ src/pages/order/index.js
@@ -1,9 +1,9 @@
 import React, { PureComponent } from 'react';
-import ProductList from "../../components/ProductList";
+import ProductList from '../../components/ProductList';
 import { NavBar, Icon } from 'antd-mobile';
 import router from 'umi/router';
 
-import { connect } from "dva";
+import { connect } from 'dva';
 
 import styles from './index.css';
 
@@ -12,17 +12,15 @@ import styles from './index.css';
 }))
 class Order extends PureComponent {
   componentWillMount() {
-    const { dispatch,order:{list} } = this.props;
+    const { dispatch } = this.props;
 
-    if(!list.length){
-      dispatch({
-        type: 'order/getList',
-        payload: {
-          currentPage:1,
-          pageSize:20,
-        },
-      });
-    }
+    dispatch({
+      type: 'order/getList',
+      payload: {
+        currentPage: 1,
+        pageSize: 20,
+      },
+    });
   }
 
   handleBack() {
@@ -32,19 +30,15 @@ class Order extends PureComponent {
   render() {
     const { order } = this.props;
 
-    const productListProps={
-      list:order.list.map(item=>item.product),
-    }
+    const productListProps = {
+      list: order.list.map(item => item.product),
+    };
 
     return (
       <div className={styles.content}>
-      <NavBar
-        mode="light"
-        icon={<Icon type="left" />}
-        onLeftClick={() => this.handleBack()}
-      >
-        订购历史
-      </NavBar>
+        <NavBar mode="light" icon={<Icon type="left" />} onLeftClick={() => this.handleBack()}>
+          订购历史
+        </NavBar>
         <div className={styles.listBody}>
           <ProductList {...productListProps} />
         </div>
